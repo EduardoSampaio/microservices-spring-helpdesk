55c320b4512c42545e1db03f70423ce886637acc
Eduardo <edwardviana@gmail.com>
2025-08-10 18:55:27 -0300

    feat: add entity user

---
+++ user-service-api/src/main/java/br/com/dev/userserviceapi/entity/User.java
@@ -0,0 +1,26 @@
+package br.com.dev.userserviceapi.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+import models.enums.ProfileEnum;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Set;
+
+@With
+@Data
+@Document
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    private String id;
+    private String name;
+    private String password;
+    private String email;
+    private Set<ProfileEnum> profiles;
+}
